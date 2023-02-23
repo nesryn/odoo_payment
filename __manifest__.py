5e0f565819bf44768f8843d65edfcfa403fc1ce4
Nesrine-Essaies <nesrineessaies@gmail.com>
2023-02-23 21:46:39 +0100

    test

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "odoo payment by nesrine",
+    'name': "odoo payment by nesrine2",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
--- models/models.py
+++ models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import base64
+import json
 
 from odoo import models, fields, api,_
 from collections import defaultdict
@@ -8,26 +9,29 @@ from datetime import date, datetime, time
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
-    allocated_amount = fields.Monetary(string='allocated_amount')
+    allocated_amount = fields.Monetary(string='allocated_amount', readonly=False)
 
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
-    my_method = fields.Boolean(default=False)
-    invoice_lines = fields.Many2many("account.move",)
+    my_method = fields.Boolean(default=False,)
+    invoice_lines = fields.Many2many("account.move")
     amount_total = fields.Monetary(string='Amount total' )
     amount_residual = fields.Monetary(string='amount_residual' )
+
 
     @api.onchange('partner_id')
     def onchange_invoice_lines(self):
         for rec in self:
+            print(self.id)
             if rec.partner_id:
                 invoice_ids = self.env['account.move'].sudo().search(
-                    [('partner_id', '=',rec.partner_id.id)])
+                    [('partner_id', '=',rec.partner_id.id),('move_type','in',('out_invoice','in_invoice')),('state','=','posted'),('payment_state','in',('not_paid','partial'))])
                 rec.sudo().write({"invoice_lines": [(6, 0, invoice_ids.ids)]})
+                rec.sudo().write({'reconciled_invoice_ids': [(6, 0, self.invoice_lines.ids)]})
 
-                if invoice_ids.ids != []:
-                    rec.my_method=True
+
+
 
     @api.onchange('invoice_lines')
     def onchange_invoice_lines_sum(self):
@@ -42,19 +46,31 @@ class AccountPayment(models.Model):
             rec.sudo().write({"amount_total": sum})
             rec.sudo().write({"amount_residual": sum2})
             rec.sudo().write({"amount": sum_allocated_amount})
-
-
+            if  rec.invoice_lines:
+                rec.my_method = True
 
     def action_post2(self):
         if self.invoice_lines:
+            for rec in self.invoice_lines:
+                vals = {'amount': rec.allocated_amount, 'date': datetime.today(),
+                     # 'communication': invoice.name,
+                     # 'has_invoices': True,
+                     # 'invoice_ids': [(6, 0, [int(invoice.id)])]
+                }
 
-            self.write(
-                {'amount': self.amount, 'date': datetime.today(),
-                 # 'communication': invoice.name,
-                 'partner_type': 'customer',
-                 # 'has_invoices': True,
-                 # 'invoice_ids': [(6, 0, [int(invoice.id)])]}
-                 'reconciled_invoice_ids': [(6, 0, self.invoice_lines.ids)]}
-            )
+                if self.payment_type == "inbound":
+                    vals['reconciled_invoice_ids']= [(4, rec.id)]
+                self.sudo().write(vals)
+                for inv in self.invoice_lines:
+                    try:
+
+                        inv.payment_id=self.id
+                        invoice_id = self.env['account.move'].sudo().search(
+                            [('name', '=', inv.invoice_outstanding_credits_debits_widget)])
+                        invoice_outstanding_credits_debits_widget2=json.loads(inv.invoice_outstanding_credits_debits_widget)
+                        id_cont =invoice_outstanding_credits_debits_widget2['content'][0]['id']
+                        inv.js_assign_outstanding_line(id_cont)
+                    except Exception as E:
+                        pass
+            #
             self.action_post()
-
